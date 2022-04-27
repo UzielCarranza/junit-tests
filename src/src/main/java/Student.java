eb6ea573bb638ccd794d68e3ac9184119034b0c5
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-27 15:56:36 -0400

    adding average to method

--- src/src/main/java/Student.java
+++ src/src/main/java/Student.java
@@ -1,8 +1,40 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
     long studentId;
-    String studentName;
+    String name;
     ArrayList<Integer> grades;
 
+    public Student() {
+    }
+
+    public long getId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public double getGradeAverage(){
+        Object[] arrayName = this.grades.toArray();
+        Arrays.sort(arrayName);
+        double median;
+        if (arrayName.length % 2 == 0) {
+            median = ((double) arrayName[arrayName.length / 2] + (double) arrayName[arrayName.length / 2 - 1]) / 2;
+        } else {
+            median = (double) arrayName[arrayName.length / 2];
+        }
+        return median;
+    }
 }
