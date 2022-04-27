c6f28a65b9f481d2dd5ded338740599e14a538bb
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-27 17:30:34 -0400

    initial set up for testing

--- src/test/java/cohorttest.java
+++ src/test/java/cohorttest.java
@@ -4,8 +4,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-
 public class cohorttest {
 
+
+    @Test
+    public void testAddStudent(){
+        Cohort cohort = new Cohort();
+        Student student = new Student(0,"test");
+        cohort.addStudent(student);
+
+
+    };
 
 }
