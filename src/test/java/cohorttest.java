76397787f17d82c085efb855284f5f95a92d5bc8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-28 16:26:27 -0400

    tested instance on students to cohorts

--- src/test/java/cohorttest.java
+++ src/test/java/cohorttest.java
@@ -7,13 +7,28 @@ import static org.junit.Assert.*;
 public class cohorttest {
 
 
+//    @Test
+//    public void testAddStudent(){
+//        Cohort cohort = new Cohort();
+//        Student student = new Student(0,"test");
+//        cohort.addStudent(student);
+//        assertNotNull(student.getGrades());
+//        assertNotNull(student);
+//    };
+
+
     @Test
-    public void testAddStudent(){
-        Cohort cohort = new Cohort();
-        Student student = new Student(0,"test");
-        cohort.addStudent(student);
+    public void addInstanceOfStudent(){
+        Cohort fortran = new Cohort();
+
+        fortran.addStudent(new Student(5,"uziel"));
+        assertNotNull(fortran.getStudents());
 
 
-    };
+//        student name added
+        assertEquals("uziel", fortran.getStudents().get(0).getName());
+
+
+    }
 
 }
