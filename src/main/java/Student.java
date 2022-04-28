6eb3d67034c29247bc1af983ef7be184d8b27491
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-28 16:15:09 -0400

    setting up files

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,45 +1,38 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Student {
-    int id;
-    String name;
-    ArrayList<Integer> grades;
 
-    public int getId() {
-        return id;
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public long getId() {
+        return this.id;
     }
 
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        return this.name;
     }
 
     public ArrayList<Integer> getGrades() {
-        return grades;
+        return this.grades;
     }
 
-    public void setGrades(ArrayList<Integer> grades) {
-        this.grades = grades;
+    public void addGrade(int grade) {
+        this.grades.add(grade);
     }
 
-    public void addGrade(int i) {
-        grades = new ArrayList<>();
-        grades.add(i);
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size() ;
     }
-
-    //    public double getGradeAverage() {
-//        int total = this.grades.stream().reduce(0, (sum, currentGradeElement) -> {
-//            return sum + currentGradeElement;
-//        });
-//        return (double) total / this.grades.size();
-//    }
-
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,45 +1,45 @@
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class StudentTest {
-
-
-    @Test
-    public void studentTest() {
-        Student student = new Student();
-
-    }
-
-//    @Test
-//    public void StudentTestAverage(){
-//        for (int i = 0; i < ; i++) {
+//import org.junit.Test;
 //
-//        }
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//public class StudentTest {
+//
+//
+//    @Test
+//    public void studentTest() {
+//        Student student = new Student();
 //
 //    }
-
-
-    @Test
-    public void getClassAverage_GivenValidInput_ReturnClassAverage() {
-        List<Student> students = new ArrayList<>();
-
-        int[] grades = {0, 50, 100};
-        int modifier = 20;
-        for (int i = 0; i <= 3; i++) {
-            Student student = new Student();
-
-            if (i == 2) {
-                student.addGrade(80);
-            } if (i == 0) {
-                student.addGrade(75);
-            }
-            if (i == 1){
-                student.addGrade(53);
-            }
-            student.addGrade(2);
-        }
-    }
-}
+//
+////    @Test
+////    public void StudentTestAverage(){
+////        for (int i = 0; i < ; i++) {
+////
+////        }
+////
+////    }
+//
+//
+//    @Test
+//    public void getClassAverage_GivenValidInput_ReturnClassAverage() {
+//        List<Student> students = new ArrayList<>();
+//
+//        int[] grades = {0, 50, 100};
+//        int modifier = 20;
+//        for (int i = 0; i <= 3; i++) {
+//            Student student = new Student();
+//
+//            if (i == 2) {
+//                student.addGrade(80);
+//            } if (i == 0) {
+//                student.addGrade(75);
+//            }
+//            if (i == 1){
+//                student.addGrade(53);
+//            }
+//            student.addGrade(2);
+//        }
+//    }
+//}
