ee3f8499cb2a0a75285b2d768ae72a8da4d25d54
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-28 17:18:04 -0400

    correct average

--- src/test/java/cohorttest.java
+++ src/test/java/cohorttest.java
@@ -37,6 +37,27 @@ public class cohorttest {
         assertFalse("is not empty", fortran.getStudents().isEmpty());
     }
 
+    @Test
+    public void correctAverage(){
+        Cohort cohort = new Cohort();
+        Student s1 = new Student(0,"test");
+        Student s2 = new Student(1,"test2");
+        s1.addGrade(5);
+        s1.addGrade(10);
+        s1.addGrade(7);
+        s2.addGrade(12);
+        s2.addGrade(5);
+        s2.addGrade(6);
+
+        cohort.addStudent(s1);
+        cohort.addStudent(s2);
+
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertEquals("5 is greater than 4", 7.5, cohort.getCohortAverage(), 0.0);
+    }
 
 
 }
